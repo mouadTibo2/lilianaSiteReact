bf17e449fc0a7e9a4ae2219ab1f9d91c475ea552
mouadTibo2 <Tibouqalinemouad@gmail.com>
2024-07-02 18:38:35 +0100

    7-2-2024

--- src/components/Admin/CrudCatalogue.tsx
+++ src/components/Admin/CrudCatalogue.tsx
@@ -1,62 +1,73 @@
 import DataTable from 'react-data-table-component';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import axios from 'axios';
 import Button from 'react-bootstrap/Button';
 import Figure from 'react-bootstrap/Figure';
-import {useEffect, useState} from 'react';
+import { useEffect, useState} from 'react';
 import trash from "../../../public/images/trash3-fill.svg";
 import update from "../../../public/images/pencil.svg";
 import view from "../../../public/images/eye-fill.svg"
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import Form from 'react-bootstrap/Form';
 import AdminNavBar from './AdminNavBar';
+import { useNavigate } from 'react-router-dom';
+
 interface dataProps{
   id: number;
   imagePath: string;
 }
-const customStyles = {
-	rows: {
-		style: {
-			minHeight: 'auto', // override the row height
-		},
-	},
-	headCells: {
-		style: {
-			paddingLeft: '10px', // override the cell padding for head cells
-			paddingRight: '8px',
-      
-		},
-	},
-	cells: {
-		style: {
-			paddingLeft: '10px', // override the cell padding for data cells
-			paddingRight: '10px',
-		},
-	},
-};
-const paginationComponentOptions = {
-	rowsPerPageText: 'Max de page',
-	rangeSeparatorText: 'de',
-	selectAllRowsItem: false,
-	selectAllRowsItemText: 'Todos',
-};
-
 
 const CrudCatalogue = () => {
-  const [showView, setShowView] = useState(false);
-  const handleCloseView = () => setShowView(false);
-  const handleShowView = () => setShowView(true);
-  const [showNew, setShowNew] = useState(false);
-  const handleCloseNew = () => setShowNew(false);
-  const handleShowNew = () => setShowNew(true);
-
+  /* usestate */
+  const navigate = useNavigate()
+  const [show, setShow] = useState(false);
   const [data ,setData] = useState( [] as dataProps[]);
   const [row ,setRow] = useState(data);
+  const [records, setRecords] = useState([] as dataProps[])
+  
+  /* data useState */
+  const [id, setId] = useState<number>();
+  const [imagePath, setImagePath] = useState<string>();
+  /* functions */
+  const handleChangeSelected = ({ selectedRows }) => {
+       console.log('Selected Rows: ', selectedRows);
+  };
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const handelFilter = (e:React.ChangeEvent<HTMLInputElement>) =>{
+    (e.target.value) == "" ?
+      setRecords(data)
+      :
+      setRecords(
+        data.filter((f) => {
+          return f.id.toString().includes(e.target.value)})
+      )
+  }
+  const handelDelete = (id:number) => {
+    console.log(id)
+    const confirm = window.confirm("vous voulez vraiment supprimer !!!")
+    if (confirm === true) {
+      axios
+        .delete(`http://localhost:3000/catalogueData/${id}`)
+        .then((res)=>{
+          alert("supprission autoriser") ;
+          navigate(0);
+        })
+    }else{
+      alert("supprission annuler");
+    }
+  }
+  const handelView = (id:number) => {
+    setId(id);
+    records.filter(f =>{
+      return (f.id === id ? setImagePath(f.imagePath):null)
+    })
+    handleShow();
+  }
+   /* data-table data structure */
   const columns = [
     {
-      name: 'Id',
+      name: 'id',
       selector: row => row.id,
       sortable: true,
     },
@@ -83,45 +94,95 @@ const CrudCatalogue = () => {
       sortable: true,
       cell: (row) => (
         <>
-          <Button variant="primary" onClick={handleShowView}><img src={view} alt="" /></Button>
+          <Button variant="primary" onClick={() =>handelView(row.id)}><img src={view} alt="" /></Button>
           <Button variant="success" className='mx-2'><img src={update} alt="" /></Button>
-          <Button variant="danger"><img src={trash} alt="" /></Button>
+          <Button variant="danger" onClick={() =>handelDelete(row.id)}><img src={trash} alt="" /></Button>
         </>
         
       ),
     }
   ];
-  const handleChangeSelected = ({ selectedRows }) => {
-       console.log('Selected Rows: ', selectedRows);
+  const customStyles = {
+    rows: {
+      style: {
+        minHeight: 'auto', // override the row height
+      },
+    },
+    headCells: {
+      style: {
+        paddingLeft: '10px', // override the cell padding for head cells
+        paddingRight: '8px',
+        
+      },
+    },
+    cells: {
+      style: {
+        paddingLeft: '10px', // override the cell padding for data cells
+        paddingRight: '10px',
+      },
+    },
   };
+  const paginationComponentOptions = {
+    rowsPerPageText: 'Max de page',
+    rangeSeparatorText: 'de',
+    selectAllRowsItem: false,
+    selectAllRowsItemText: 'Todos',
+  };
+  /* data requests */ 
   useEffect(()=>{
     axios
-    .get('http://localhost:3000/catalogueData')
-    .then(res=>{setData(res.data)})
+    .get(`http://localhost:3000/catalogueData/`)
+    .then(res=>{setData(res.data)
+      setRecords(res.data);
+    })
     .catch(error=> console.log(error))
   },[]);
 
-  const offCanvaView =data.map(item =>{
-		return (
-			<>
-			<Offcanvas show={showView} onHide={handleCloseView}>
-				<Offcanvas.Header closeButton>
-				<Offcanvas.Title>View</Offcanvas.Title>
-				</Offcanvas.Header>
-				<Offcanvas.Body>
-				<Form.Label>Id de Formation</Form.Label>
+ return (
+    <>
+    <AdminNavBar/>
+    <Container>
+      <h1 className="text-center fs-1 my-3 fw-normal">Gestion Catalogue</h1>
+      <DataTable
+        columns={columns}
+        data={records}
+        customStyles={customStyles}
+        pagination
+        paginationComponentOptions={paginationComponentOptions}
+        selectableRows
+        onSelectedRowsChange={handleChangeSelected}
+        actions={
+          <Button variant="primary" onClick={handleShow}>Nouveau photo</Button>
+        }
+        subHeader
+        subHeaderComponent={
           <Form.Control
-            disabled
-            type="text"
-            placeholder="First name"
-            value={item.id}
+            placeholder='Chercher par id'
+            className='w-25 me-auto mb-3 shadow-sm'
+            onChange={handelFilter}
+            
           />
-          <Form.Label>Titre de Formation</Form.Label>
+        }
+        />
+        <Offcanvas show={show} onHide={handleClose}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Nouveau Photo</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+        <Form.Label>Id de Formation</Form.Label>
           <Form.Control
+            type="number"
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+            required
             disabled
+          />
+          <Form.Label>chemin d'image</Form.Label>
+          <Form.Control
             type="text"
-            placeholder="First name"
-            value={item.imagePath}
+            value={imagePath}
+            onChange={(e) => setImagePath(`../../../images/${e.target.value}`)}
+            required
           />
           <Form.Label>Image</Form.Label>
           <div>
@@ -130,59 +191,12 @@ const CrudCatalogue = () => {
                 width={171}
                 height={170}
                 alt="171x180"
-                src={item.imagePath}
+                src={imagePath}
               />
             </Figure>
           </div>
-          </Offcanvas.Body>
-			</Offcanvas>
-			</>
-		);
-	})
-
-  return (
-    <>
-    <AdminNavBar/>
-    <Container>
-    <Row>
-      <Col lg={12} className="text-center fs-1 my-3 fw-normal">Gestion Catalogue</Col>
-      <Col lg={12}>
-        <DataTable
-        columns={columns}
-        data={data}
-        customStyles={customStyles}
-        pagination
-        paginationComponentOptions={paginationComponentOptions}
-        selectableRows
-        onSelectedRowsChange={handleChangeSelected}
-        actions={
-          <Button variant="primary" onClick={handleShowNew}>Nouveau photo</Button>
-        }
-        />
-      </Col>
-    </Row>
-    <>
-      <Offcanvas show={showNew} onHide={handleCloseNew}>
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Nouveau Photo</Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-        <Form.Label>Id de Formation</Form.Label>
-          <Form.Control
-            type="text"
-            value=""
-          />
-          <Form.Label>chemin d'image</Form.Label>
-          <Form.Control
-            type="text"
-            value=""
-          />
-         
         </Offcanvas.Body>
       </Offcanvas>
-
-      {offCanvaView}
-    </>
   </Container>
   </>
   )
--- src/components/Admin/CrudFormation.tsx
+++ src/components/Admin/CrudFormation.tsx
@@ -11,10 +11,11 @@ import trash from "../../../public/images/trash3-fill.svg";
 import update from "../../../public/images/pencil.svg";
 import view from "../../../public/images/eye-fill.svg"
 import AdminNavBar from './AdminNavBar';
+import { useNavigate } from 'react-router-dom';
 import { useEffect, useState} from 'react'
 
 interface dataProps {
-  idFormation: number,
+  id: number,
   titreformation:string,
   imageFormation: string,
   descriptionFormation: string,
@@ -27,52 +28,76 @@ interface dataProps {
   ]
 }
   
-const customStyles = {
-	rows: {
-		style: {
-			minHeight: '80px', // override the row height
-		},
-	},
-	headCells: {
-		style: {
-			paddingLeft: '10px', // override the cell padding for head cells
-			paddingRight: '8px',
-      
-		},
-	},
-	cells: {
-		style: {
-			paddingLeft: '10px', // override the cell padding for data cells
-			paddingRight: '10px',
-		},
-	},
-};
 
-const paginationComponentOptions = {
-	rowsPerPageText: 'Max de page',
-	rangeSeparatorText: 'de',
-	selectAllRowsItem: false,
-	selectAllRowsItemText: 'Todos',
-};
-const handleChangeSelected = ({ selectedRows }) => {
-  console.log('Selected Rows: ', selectedRows);
-};
 const Crud = () => {
-  const [showView, setShowView] = useState(false);
-  const handleCloseView = () => setShowView(false);
-  const handleShowView = () => setShowView(true);
-  const [showNew, setShowNew] = useState(false);
-  const handleCloseNew = () => setShowNew(false);
-  const handleShowNew = () => setShowNew(true);
-  
-  
-
+  /* usestates */
+  const navigate = useNavigate()
+  const [show, setShow] = useState(false);
   const [data ,setData] = useState( [] as dataProps[]);
   const [row ,setRow] = useState(data);
+  const [records, setRecords] = useState([] as dataProps[]);
+
+  /* data useState */
+  const [id, setId] = useState<number>();
+  const [titreformation, setTitreformation] = useState<string>();
+  const [imageFormation, setImageFormation] = useState<string>();
+  const [descriptionFormation, setDescriptionFormation] = useState<string>();
+  const [modules, setModules] = useState({
+    moduleId: 0,
+    moduleNom:"",
+    moduleImage: "",
+    moduleObjectif: "",
+  });
+   /* functions */
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const handelFilter = (e:React.ChangeEvent<HTMLInputElement>) =>{
+    (e.target.value) == "" ?
+      setRecords(data)
+      :
+      setRecords(
+        data.filter((item) => {
+         return item.titreformation.toLowerCase().includes(e.target.value)} )
+      )
+      console.log(data.filter((item) => {item.titreformation.toLowerCase()}),"filter")
+      console.log(records)
+      console.log(e.target.value)
+  }
+  const handelDelete = (id:number) => {
+    const confirm = window.confirm("vous voulez vraiment supprimer !!!")
+    if (confirm === true) {
+      axios
+        .delete(`http://localhost:3000/formationData/${id}/`)
+        .then((res)=>{
+          alert("supprission autoriser") ;
+          navigate(0);
+        })
+    }else{
+      alert("supprission annuler");
+    }
+  }
+  const handelView = (id:number) => {
+    setId(id);
+    records.filter(f =>{
+      return (f.id === id ? setTitreformation(f.titreformation):null)
+    })
+    records.filter(f =>{
+      return (f.id === id ? setImageFormation(f.imageFormation):null)
+    })
+    records.filter(f =>{
+      return (f.id === id ? setDescriptionFormation(f.descriptionFormation):null)
+    })
+    records.filter(f =>{
+      return (f.id === id ? setModules(f.modules):null)
+    })
+    handleShow();
+    console.log(f.modules);
+  }
+  /* data-table data structure */
   const columns = [
     {
-      name: 'idFormation',
-      selector: row => row.idFormation,
+      name: 'id',
+      selector: row => row.id,
       sortable: true,
     },
     {
@@ -113,107 +138,54 @@ const Crud = () => {
       sortable: true,
       cell: (row) => (
         <>
-          <Button variant="primary" onClick={handleShowView}><img src={view} alt="" /></Button>
+          <Button variant="primary" onClick={(e) =>handelView(row.id)}><img src={view} alt="" /></Button>
           <Button variant="success" className='mx-2'><img src={update} alt="" /></Button>
-          <Button variant="danger"><img src={trash} alt="" /></Button>
+          <Button variant="danger" onClick={(e) =>handelDelete(row.id)}><img src={trash} alt="" /></Button>
         </>
         
       ),
     }
   ];
+  const customStyles = {
+    rows: {
+      style: {
+        minHeight: '80px', // override the row height
+      },
+    },
+    headCells: {
+      style: {
+        paddingLeft: '10px', // override the cell padding for head cells
+        paddingRight: '8px',
+        
+      },
+    },
+    cells: {
+      style: {
+        paddingLeft: '10px', // override the cell padding for data cells
+        paddingRight: '10px',
+      },
+    },
+  };
   
-
+  const paginationComponentOptions = {
+    rowsPerPageText: 'Max de page',
+    rangeSeparatorText: 'de',
+    selectAllRowsItem: false,
+    selectAllRowsItemText: 'Todos',
+  };
+  const handleChangeSelected = ({ selectedRows }) => {
+    console.log('Selected Rows: ', selectedRows);
+  };
+  /* data requests */ 
   useEffect(()=>{
     axios
     .get('http://localhost:3000/formationData')
-    .then(res=>{setData(res.data)})
+    .then(res=>{setData(res.data)
+      setRecords(res.data);
+    })
     .catch(error=> console.log(error))
   },[]);
-  const offCanvaView =data.map(item =>{
-		return (
-			<>
-			<Offcanvas show={showView} onHide={handleCloseView}>
-				<Offcanvas.Header closeButton>
-				<Offcanvas.Title>View</Offcanvas.Title>
-				</Offcanvas.Header>
-				<Offcanvas.Body>
-				<Form.Label>Id de Formation</Form.Label>
-          <Form.Control
-            disabled
-            type="text"
-            placeholder="First name"
-            value={item.idFormation}
-          />
-          <Form.Label>Titre de Formation</Form.Label>
-          <Form.Control
-            disabled
-            type="text"
-            placeholder="First name"
-            value={item.titreformation}
-          />
-          <Form.Label>Description Formation</Form.Label>
-          <Form.Control
-            disabled
-            type="text"
-            maxLength={20}
-            placeholder="First name"
-            value={item.descriptionFormation}
-          />
-          <Form.Label className='mt-3'>Image</Form.Label>
-          <div>
-            <Figure className='mt-3'>
-              <Figure.Image
-                width={171}
-                height={170}
-                alt="171x180"
-                src={item.imageFormation}
-              />
-            </Figure>
-          </div>
-          <Form.Label>Modules</Form.Label>
-          <div>
-          {item.modules.map(item =>{
-            return (<>
-              <Form.Label>Module {item.moduleId}</Form.Label>  
-              <Form.Control
-                disabled
-                type="text"
-                placeholder="First name"
-                value={item.moduleId}
-              />
-              <Form.Control
-                disabled
-                type="text"
-                value={item.moduleNom}
-              />
-              <Form.Control
-                disabled
-                type="text"
-                value={item.moduleImage}
-              />
-              <div>
-                <Figure className='mt-3'>
-                  <Figure.Image
-                    width={171}
-                    height={170}
-                    alt="171x180"
-                    src={item.moduleImage}
-                  />
-                </Figure>
-              </div>
-              <Form.Control
-                disabled
-                type="textarea"
-                value={item.moduleObjectif}
-              />
-            </>);})
-          }
-          </div>
-				</Offcanvas.Body>
-			</Offcanvas>
-			</>
-		);
-	});
+  
 
   return (
     <>
@@ -224,69 +196,80 @@ const Crud = () => {
       <Col lg={12}>
         <DataTable
         columns={columns}
-        data={data}
+        data={records}
         customStyles={customStyles}
         pagination
         paginationComponentOptions={paginationComponentOptions}
         selectableRows
         onSelectedRowsChange={handleChangeSelected}
-        actions={
-          <Button variant="primary" onClick={handleShowNew}>Nouveau Formation</Button>
+       actions={
+          <Button variant="primary" onClick={handleShow}>Nouveau Formation</Button>
+        }
+        subHeader
+        subHeaderComponent={
+          <Form.Control
+            placeholder='Chercher une formation'
+            className='w-25 me-auto mb-3 shadow-sm'
+            onChange={handelFilter}
+          />
         }
         />
       </Col>
     </Row>
     <>
-      <Offcanvas show={showNew} onHide={handleCloseNew}>
+      <Offcanvas show={show} onHide={handleClose}>
         <Offcanvas.Header closeButton>
           <Offcanvas.Title>Nouveau Formation</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
-        <Form.Label>Id de Formation</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="First name"
-            value=""
-          />
-          <Form.Label>Titre de Formation</Form.Label>
+        <Form.Label>Id</Form.Label>
           <Form.Control
             disabled
             type="text"
             placeholder="First name"
-            value=""
+            value={id}
           />
-          <Form.Label>Description Formation</Form.Label>
+          <Form.Label>Titre de Formation</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="First name"
+            value={titreformation}
+          />
+          
+          <Form.Label>image Pahth</Form.Label>
           <Form.Control
             type="text"
             maxLength={20}
             placeholder="First name"
-            value=""
-          />
-          <Form.Label>Date de Naissaince</Form.Label>
-          <Form.Control
-            type="text"
-            maxLength={20}
-            placeholder="First name"
-            value=""
+            value={imageFormation}
           />
           <Form.Label>image</Form.Label>
-          <Figure className='mt-3'>
-            <Figure.Image
-              width={171}
-              height={170}
-              alt="171x180"
-              src=""
-            />
-        </Figure>
+          <div>
+            <Figure className='mt-3'>
+              <Figure.Image
+                width={171}
+                height={170}
+                alt="171x180"
+                src={imageFormation}
+              />
+            </Figure>
+          </div>
+        <Form.Label>Description Formation</Form.Label>
+          <Form.Control
+            type="text"
+            maxLength={20}
+            placeholder="First name"
+            value={descriptionFormation}
+          />
           <Form.Label>Modules</Form.Label>
           <Form.Control
             type="text"
             placeholder="First name"
-            value=""
+            value={modules}
           />
         </Offcanvas.Body>
       </Offcanvas>
-      {offCanvaView}
+   
     </>
   </Container>
   </>
